fb39a603f1bf45468761e62dced0f75f70ad0ac5
Vikash <vikashpathak041@gmail.com>
2022-07-16 19:14:37 +0530

    Adress book system uc1

---
+++ AdressBook.js
@@ -0,0 +1,34 @@
+class Contact{
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;
+
+    constructor(...params)
+    {
+        this.firstName = params[0];
+        this.lastName = params[1];
+        this.address = params[2];
+        this.city = params[3];
+        this.state = params[4];
+        this.zip = params[5];
+        this.phone = params[6];
+        this.email = params[7];
+    }
+
+
+    toString()
+    {
+        return "FirstName = "+this.firstName+", LastName = "+this.lastName+", Address = "+this.address+
+                ", City = "+this.city+", State = "+this.state+", Zip = "+this.zip+", Phone = "+this.phone+", Email = "+this.email;
+    }
+
+}
+
+let contact = new Contact("vikash","pathak","jasola saheen bagh","delhi","delhi",829064,9931633988,"vikashpathak041@gmail.com");
+console.log(contact.toString());
